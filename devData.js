1f2cd860b3052d5a8c22928d6d5320b5d28e841e
Amy Gori <amydgori@gmail.com>
2022-08-04 22:25:44 -0400

    Add data

---
+++ devData.js
@@ -0,0 +1,58 @@
+devs = [
+  {
+    name: 'Andres Alcocer',
+    description:
+      'Andres worked with another back-end developer and two front-end teams to build an API for a Stack Overflow clone application called QuestionBox.',
+    repoURL:
+      'https://github.com/Momentum-Team-13/social-e-cards-team-sugar-gliders-backend',
+    videoURL: '',
+  },
+  {
+    name: 'An Le',
+    description:
+      'An worked with two other front-end developers and a back-end team to build a social greeting card sharing app.',
+    repoURL:
+      'https://github.com/Momentum-Team-13/social-e-cards-team-quokka-front-end',
+    videoURL: '',
+  },
+  {
+    name: 'Chloe Mintel',
+    description:
+      'Chloe worked with another front end developer and a back-end team to build a plant-themed Q&A platform called PlantSpace.',
+    repoURL:
+      'https://github.com/Momentum-Team-13/questionbox-team-front-end-plantspace',
+    videoURL: '',
+  },
+  {
+    name: 'Daniela Ortiz-Kosobucki',
+    description:
+      'Daniela worked with Mike Perry and a back-end team that included Andres Alcocer to create a social greeting card sharing app.',
+    repoURL:
+      'https://github.com/Momentum-Team-13/social-e-cards-front-end-team-sugar-gliders',
+    videoURL: '',
+  },
+  {
+    name: 'Mike Perry',
+    description:
+      'Mike worked with Daniela Ortiz-Kosobucki and a back-end team that included Andres Alcocer to build a social greeting card sharing app.',
+    repoURL:
+      'https://github.com/Momentum-Team-13/social-e-cards-front-end-team-sugar-gliders',
+    videoURL: '',
+  },
+  {
+    name: 'Jose Reyes',
+    description:
+      'Jose worked with another developer to create an 80s-inspired video-game-themed Q&A platform.',
+    repoURL:
+      'https://github.com/Momentum-Team-13/questionbox-team-read-pand-front-end',
+    videoURL: '',
+  },
+  {
+    name: 'Lisa Williams',
+    description:
+      'Lisa worked with another front-end developer and a back-end team that included Andres Alcocer to build a social greeting card sharing app.',
+    repoURL:
+      'https://github.com/Momentum-Team-13/social-e-cards-team-lion-sugar-gliders-front-end',
+    videoURL: '',
+  },
+]
---
+++ script.js
@@ -0,0 +1,19 @@
+devListSection = document.getElementById('dev-list')
+
+for (let dev of devs) {
+  devEl = document.createElement('div')
+  devEl.classList.add('list-item', 'box')
+  devEl.innerHTML = `
+  <div class="list-item-content">
+  <div class="list-item-title">${dev.name}</div>
+  <div class="list-item-description">
+    <p class="block">${dev.description}</p>
+    <p>
+      <a href="${dev.repoURL}">GitHub Repo</a>
+    </p>
+    <p><a href="${dev.videoURL}">Link to video presentation</a></p>
+  </div>
+</div>
+  `
+  devListSection.appendChild(devEl)
+}
